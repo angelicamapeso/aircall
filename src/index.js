2d4d9a1089acfeb48ef0e8c41e2dda2ed7ced21b
Angelica Mapeso <angelica.mapeso@gmail.com>
2023-07-26 20:46:59 -0400

    use react 18 render, move css style imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import './css/body.css';
-import './css/app.css';
-import './css/header.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx';
+import App from "./App.jsx";
+import "./css/body.css";
 
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
