4ef318da052c43e75369b4885248b3bfbbef31db
Angelica Mapeso <angelica.mapeso@gmail.com>
2023-07-27 13:33:46 -0400

    add icons, style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import AircallLogo from "../icons/aircall.svg";
 import "../styles/Header.scss";
 
 const Header = () => {
   return (
     <header>
+      <div id="logo">
+        <img src={AircallLogo} />
+      </div>
       <nav>
         <ul>
           <li>
-            <NavLink to="/">Calls</NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Calls
+            </NavLink>
           </li>
           <li>
-            <NavLink to="archived">Archived</NavLink>
+            <NavLink
+              to="archived"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Archived
+            </NavLink>
           </li>
         </ul>
       </nav>
