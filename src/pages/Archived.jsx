49ff4572bfdc41508944da24620c98a569cfdf58
Angelica Mapeso <angelica.mapeso@gmail.com>
2023-07-27 19:17:53 -0400

    complete archived page

--- src/pages/Archived.jsx
+++ src/pages/Archived.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { CallContext } from "../context/CallContext";
+
+import CallEntry from "../components/CallEntry.jsx";
+import UnarchiveIcon from "../icons/unarchive.svg";
+
+import "../styles/archived.scss";
 
 const Archived = () => {
+  const { archivedCalls, getAllCalls } = useContext(CallContext);
+
+  useEffect(() => {
+    getAllCalls();
+  }, []);
+
+  useEffect(() => {
+    console.log(archivedCalls);
+  }, [archivedCalls]);
+
   return (
     <div id="archived" className="container-view">
-      <p>Welcome to the archived page!</p>
+      <div className="unarchive-section">
+        <button className="btn-blue">
+          <img src={UnarchiveIcon} />
+          Unarchive All Calls
+        </button>
+      </div>
+      <ul>
+        {archivedCalls.map((call) => (
+          <li key={call.id}>
+            <CallEntry call={call} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
